ac49de0ec4c824f18f26133060187b2fbdc3484b
k4m4 <nikolaskam@gmail.com>
2020-09-25 20:20:06 +0300

    Move favicons to /static/images/favicons

--- components/meta.js
+++ components/meta.js
@@ -16,9 +16,10 @@ const Meta = () => {
 			<meta property="twitter:image" content="https://wifipass.co/static/images/logo.png" />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<link rel="stylesheet" href="/static/styles/index.css" />
-			<link rel="apple-touch-icon" sizes="180x180" href="/static/images/apple-touch-icon.png" />
-			<link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon-32x32.png" />
-			<link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon-16x16.png" />
+			<link rel="apple-touch-icon" sizes="180x180" href="/static/images/favicons/apple-touch-icon.png" />
+			<link rel="icon" type="image/png" sizes="192x192" href="/static/images/favicons/favicon-192x192.png" />
+			<link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicons/favicon-32x32.png" />
+			<link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicons/favicon-16x16.png" />
 			<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet" />
 		</>
 	);
